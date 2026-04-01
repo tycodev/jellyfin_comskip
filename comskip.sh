63502d77b1e81e90124f1cbc9ca6374307212be1
tycodev <15969114+tycodev@users.noreply.github.com>
2026-04-01 16:28:47 -0400

    Update script to swap the skip marker for the commercial break marker for jellyfin.

--- comskip.sh
+++ comskip.sh
@@ -10,6 +10,8 @@ while [ "$iteration" -lt "$maxRetries" ]; do
     comskipReturn=$?
     echo "Comskip returned: $comskipReturn"
     if [ "$comskipReturn" -eq "0" ]; then
+        # map the type from 0 -> 3 for jellyfin.
+        sed -Ei 's/([0-9]+\.[0-9]+[[:space:]]+[0-9]+\.[0-9]+)[[:space:]]+0$/\1 3/' "${filePath%.*}".edl
         exit 0
     fi
     iteration=$((iteration+1))
