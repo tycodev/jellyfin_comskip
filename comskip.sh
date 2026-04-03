f6e384efa078413bfb605a5e6aa95b80173e9f3a
tycodev <15969114+tycodev@users.noreply.github.com>
2026-04-03 09:27:28 -0400

    encode to tmp file then rename to mkv to avoid early detections before file is ready.

--- comskip.sh
+++ comskip.sh
@@ -13,6 +13,7 @@ while [ "$iteration" -lt "$maxRetries" ]; do
         # map the type from 0 -> 3 for jellyfin.
         sed -Ei 's/([0-9]+\.[0-9]+[[:space:]]+[0-9]+\.[0-9]+)[[:space:]]+0$/\1 3/' "${filePath%.*}".edl
 
+        tmpOutputFile="${filePath%.*}.tmp"
         outputFile="${filePath%.*}.mkv"
 
         field_order=$(/usr/bin/env ffprobe -v error -select_streams v:0 -show_entries stream=field_order -of default=nokey=1:noprint_wrappers=1 "$filePath" 2>/dev/null)
@@ -24,7 +25,7 @@ while [ "$iteration" -lt "$maxRetries" ]; do
             deint_filter=""
         fi
 
-        echo "Re-encoding '$filePath' to '$outputFile' with QSV h264 encoder"
+        echo "Re-encoding '$filePath' to '$tmpOutputFile' with QSV h264 encoder"
         if [ -n "$deint_filter" ]; then
             /usr/bin/env ffmpeg -hide_banner -loglevel info -y \
                 -hwaccel qsv -hwaccel_output_format qsv \
@@ -33,7 +34,7 @@ while [ "$iteration" -lt "$maxRetries" ]; do
                 -vf "$deint_filter" \
                 -c:v h264_qsv -preset fast -global_quality 23 \
                 -c:a copy -c:s copy \
-                "$outputFile"
+                -f matroska "$tmpOutputFile"
         else
             /usr/bin/env ffmpeg -hide_banner -loglevel info -y \
                 -hwaccel qsv -hwaccel_output_format qsv \
@@ -41,18 +42,20 @@ while [ "$iteration" -lt "$maxRetries" ]; do
                 -i "$filePath" \
                 -c:v h264_qsv -preset fast -global_quality 23 -pix_fmt yuv420p \
                 -c:a copy -c:s copy \
-                "$outputFile"
+                -f matroska "$tmpOutputFile"
         fi
         ffmpegReturn=$?
         echo "FFmpeg returned: $ffmpegReturn"
 
         if [ "$ffmpegReturn" -ne 0 ]; then
             echo "Re-encoding failed, keeping original file"
+            rm -rf "$tmpOutputFile"
             exit 1
         fi
 
         echo "Re-encoding succeeded, removing original file '$filePath'"
         rm -f "$filePath"
+        mv "$tmpOutputFile" "$outputFile"
         exit 0
     fi
     iteration=$((iteration+1))
